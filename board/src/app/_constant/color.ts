1ae9f0aae7bda1dbd031ba94af8297ab4f0a7664
kweonsikyung <ekrk6632@naver.com>
2024-07-22 16:50:03 +0900

    add: default styling set

---
+++ board/src/app/_constant/color.ts
@@ -0,0 +1,14 @@
+export const COLORS = {
+  main: '#3A4893',
+  point: '#4C77E7',
+  red: '#FE5A59',
+  bg: '#FAFAFA',
+  w: '#FFFFFF',
+  g0: '#F3F3F3',
+  g1: '#D7D7D7',
+  g2: '#AEAEAE',
+  g3: '#828282',
+  g4: '#595959',
+  g5: '#393939',
+  bk: '#0B0B0B',
+};
