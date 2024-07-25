9c32d847c93a352c043a5e919dd7d791f94f1589
kweonsikyung <ekrk6632@naver.com>
2024-07-25 13:37:47 +0900

    feat: request page & components

---
+++ board/src/app/(route)/request/utill.ts
@@ -0,0 +1,20 @@
+export const 선택한업체 = [
+  { title: 'KMTC', desc: '24.06.21(월) - 24.06.28(월)' },
+  { title: 'HMM', desc: '24.06.21(월) - 24.06.28(월)' },
+  { title: 'HMM', desc: '24.06.21(월) - 24.06.29(화)' },
+];
+
+export const 화물정보 = [
+  { title: '부산항 > 상하이항', desc: '출발지 > 도착지', isColored: true },
+  { title: '24.06.21(월)', desc: '희망출항 날짜', isColored: true },
+  { title: 'CIF', desc: '인코텀즈', isColored: true },
+  { title: '99 개', desc: '총 수출 물품 수량', isColored: true },
+  { title: '99 개', desc: '박스 당 물품 수량', isColored: true },
+  {
+    title: '123 X 456 X 789 m',
+    desc: '박스 길이 (가로x세로x높이) m',
+    isColored: false,
+  },
+  { title: '123 kg', desc: '박스 중량', isColored: false },
+  { title: '1,234,567 원', desc: '물품 가액', isColored: false },
+];
