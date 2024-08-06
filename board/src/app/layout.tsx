a9ef86aaa86136e5003b0ffe3c3dc8ccfc4ed12d
kweonsikyung <ekrk6632@naver.com>
2024-08-06 16:27:08 +0900

    add: global assets

--- board/src/app/layout.tsx
+++ board/src/app/layout.tsx
@@ -25,6 +25,14 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
           rel="stylesheet"
         />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+        />
         <title>링카고, 바다에서 시작하는 해외수출, 더 쉽고 편리하게</title>
       </head>
       <StyledComponentsRegistry>
