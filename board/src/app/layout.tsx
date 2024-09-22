af15d4f29530fb58d88eb7ff67292b99ed5abe82
kweonsikyung <ekrk6632@naver.com>
2024-09-22 18:49:45 +0900

    add: setting global fonts

--- board/src/app/layout.tsx
+++ board/src/app/layout.tsx
@@ -22,6 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link
+          rel="stylesheet"
+          as="style"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
+        />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
