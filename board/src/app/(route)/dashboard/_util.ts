fc278e5fd203ea904d7848669718b28240e3ce43
kweonsikyung <ekrk6632@naver.com>
2024-09-25 01:55:20 +0900

    feat: 대시보드 유틸 함수

---
+++ board/src/app/(route)/dashboard/_util.ts
@@ -0,0 +1,55 @@
+import { GetICompareDto } from '@/app/_apis/dashboard/getCompare';
+import { COLORS } from '@/app/_constant/color';
+import { formatDate } from '../reserve-list/utill';
+
+export const 견적_명세 = [
+  {
+    value: 'THC 비용',
+    label: 'THC 비용',
+  },
+  {
+    value: 'CIC 비용',
+    label: 'CIC 비용',
+  },
+  {
+    value: '실행 비용',
+    label: '실행 비용',
+  },
+  {
+    value: 'CFS 비용',
+    label: 'CFS 비용',
+  },
+  {
+    value: '핸들링 비용',
+    label: '핸들링 비용',
+  },
+  {
+    value: '통관 수수료',
+    label: '통관 수수료',
+  },
+];
+
+export const getCostListByType = (dummy: GetICompareDto, type: string) => {
+  switch (type) {
+    case 'THC 비용':
+      return dummy?.result.thcCostList;
+    case 'CIC 비용':
+      return dummy?.result.handlingCostList;
+    case 'CFS 비용':
+      return dummy?.result.cfsCostList;
+    case '통관 수수료':
+      return dummy?.result.listStatusCostList;
+    case '실행 비용':
+      return dummy?.result.customsClearanceCostList;
+    case '핸들링 비용':
+      return dummy?.result.truckingCostList;
+    default:
+      return [];
+  }
+};
+
+export const barColors = [COLORS.main, COLORS.point, COLORS.g3];
+
+export function formatQuoteListEl(item: any) {
+  return `${item.exportPort} → ${item.importPort} | ETD: ${formatDate(item.ETD)} (요청일: ${formatDate(item.requestDate)})`;
+}
