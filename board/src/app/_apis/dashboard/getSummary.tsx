852aea3f80f4775b2b921a21e39f380bcbfb62d4
kweonsikyung <ekrk6632@naver.com>
2024-09-24 10:44:31 +0900

    fix: 오타 수정

--- board/src/app/_apis/dashboard/getSummary.tsx
+++ board/src/app/_apis/dashboard/getSummary.tsx
@@ -11,7 +11,7 @@ export type GetISummaryDto = ResponseDto<ResultData>;
 사용자의 관심사에 따른 뉴스 정보의 요약 정보를 조회
 */
 export const getSummary = async (at: string) => {
-  const url = `/dashboards/news/summary?interests=%ED%99%98%EC%9C%A8&interests=%EC%9A%B4%EC%9E%84&interests=%EC%88%98%EC%9E%85%EA%B5%AD'`;
+  const url = `/dashboards/news/summary?interests=%ED%99%98%EC%9C%A8&interests=%EC%9A%B4%EC%9E%84&interests=%EC%88%98%EC%9E%85%EA%B5%AD`;
   const response = await getAsync<GetISummaryDto, undefined>(
     url,
     at,
