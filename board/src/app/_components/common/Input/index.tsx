36443ebf6c677c6db28cfcbce0596222048f9b35
kweonsikyung <ekrk6632@naver.com>
2024-09-25 01:54:42 +0900

    add: 공용 컴포넌트 인덱스 추가

--- board/src/app/_components/common/Input/index.tsx
+++ board/src/app/_components/common/Input/index.tsx
@@ -1,5 +1,6 @@
 import CheckboxInput from './CheckboxInput';
 import TextInput from './TextInput';
 import SelectInput from './SelectInput';
+import CustomSelectInput from './CustomSelectInput';
 
-export { CheckboxInput, TextInput, SelectInput };
+export { CheckboxInput, TextInput, SelectInput, CustomSelectInput };
