8e9ff3399b1525c44f46b9a3d74e322b3b255771
donghyuun <bomul0524@gmail.com>
2024-10-09 15:02:48 +0900

    feat: 줄바꿈 문자 적용

--- board/src/app/(route)/dashboard/(section)/MyChatHistory/index.tsx
+++ board/src/app/(route)/dashboard/(section)/MyChatHistory/index.tsx
@@ -343,7 +343,7 @@ export default function MyChatHistory() {
                     key={index}
                     type={el.senderId === userId ? 'me' : 'other'}
                   >
-                    <div>{el.content}</div>
+                    <div dangerouslySetInnerHTML={{ __html: el.content.replace(/\n/g, '<br />') }} />
                     <p>{formatDate(el.createdAt)}</p>
                   </ChatBox>
                 );
